7eb534cb32dfe8a550d0b3282cdc571d480796d3
Gihan-PC <gtheman27@gmail.com>
2021-06-09 10:25:58 -1000

    updated commit

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote form apiQuotes array
-    const quote = apiQuotes[Math.floor(math.random() * apiQuotes.length)];
-    console.log(quote);
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // check if author field is blank and replace it with 'unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // check quote length to determine styling 
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -18,6 +38,16 @@ async function getQuotes() {
     // Catch Error Here
     }
 }
+
+// Tweet a quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener( 'click', tweetQuote);
 
 // On Load
 getQuotes();
